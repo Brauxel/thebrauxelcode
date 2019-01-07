898b80b150da13b0919b6bb5fd5d28f72b2cd8ba
Aakash Bhatia <aakash@arkade.com.au>
2019-01-07 15:03:41 +0530

    index file init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('in');
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import AppContainer from './js/containers/appContainer'
+
+// Import the required CSS
+require("normalize.css/normalize.css");
+//require("./scss/index.scss");
+
+// ReactDOM.render(
+//   <AppContainer />,
+//   document.getElementById('app')
+// );
