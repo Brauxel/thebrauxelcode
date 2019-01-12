b37b70261da5de7192fd900a656626679995f75d
Aakash Bhatia <aakash@arkade.com.au>
2019-01-12 21:39:40 +0530

    grid hover state without SVGs

--- src/js/organisms/Portfolio.js
+++ src/js/organisms/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Filters from '../molecules/Filters';
 import IAmGIA from '../../assets/images/iamgia.png';
+import DetailsIcon from '../../assets/images/details-off.png';
 
 export default class extends React.Component {
     render() {
@@ -12,29 +13,100 @@ export default class extends React.Component {
 
             	<div className="grid-list make-flex align-center text-center">
             		<div className="item">
-            			<div className="hover-state">&nbsp;</div>
+            			<div className="hover-state">
+                            <div className="hover-buttons align-center space-evenly make-flex">
+                                <a href="#" className="btn btn-primary with-icon make-flex">
+                                    <span className="icon"><img src={DetailsIcon} /></span>
+                                    <span className="text">Details</span>
+                                </a>
+                                <a href="#" className="btn btn-primary">View Live</a>                                
+                            </div>
+                        </div>
+
             			<img className="main-image" src={IAmGIA} />
-            			<h5>yellow tea and flowers</h5>
-            			<p>sea, boat, photo</p>
+
+                        <div className="information">
+                            <h5>yellow tea and flowers</h5>
+                            <p>sea, boat, photo</p>
+                        </div>
             		</div>
 
-            		<div className="item">
-            			<img className="main-image" src={IAmGIA} />
-            			<h5>yellow tea and flowers</h5>
-            			<p>sea, boat, photo</p>
-            		</div>
+                    <div className="item">
+                        <div className="hover-state">
+                            <div className="hover-buttons align-center space-evenly make-flex">
+                                <a href="#" className="btn btn-primary with-icon make-flex">
+                                    <span className="icon"><img src={DetailsIcon} /></span>
+                                    <span className="text">Details</span>
+                                </a>
+                                <a href="#" className="btn btn-primary">View Live</a>                                
+                            </div>
+                        </div>
 
-            		<div className="item">
-            			<img className="main-image" src={IAmGIA} />
-            			<h5>yellow tea and flowers</h5>
-            			<p>sea, boat, photo</p>
-            		</div>
+                        <img className="main-image" src={IAmGIA} />
 
-            		<div className="item">
-            			<img className="main-image" src={IAmGIA} />
-            			<h5>yellow tea and flowers</h5>
-            			<p>sea, boat, photo</p>
-            		</div>            		
+                        <div className="information">
+                            <h5>yellow tea and flowers</h5>
+                            <p>sea, boat, photo</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="hover-state">
+                            <div className="hover-buttons align-center space-evenly make-flex">
+                                <a href="#" className="btn btn-primary with-icon make-flex">
+                                    <span className="icon"><img src={DetailsIcon} /></span>
+                                    <span className="text">Details</span>
+                                </a>
+                                <a href="#" className="btn btn-primary">View Live</a>                                
+                            </div>
+                        </div>
+
+                        <img className="main-image" src={IAmGIA} />
+
+                        <div className="information">
+                            <h5>yellow tea and flowers</h5>
+                            <p>sea, boat, photo</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="hover-state">
+                            <div className="hover-buttons align-center space-evenly make-flex">
+                                <a href="#" className="btn btn-primary with-icon make-flex">
+                                    <span className="icon"><img src={DetailsIcon} /></span>
+                                    <span className="text">Details</span>
+                                </a>
+                                <a href="#" className="btn btn-primary">View Live</a>                                
+                            </div>
+                        </div>
+
+                        <img className="main-image" src={IAmGIA} />
+
+                        <div className="information">
+                            <h5>yellow tea and flowers</h5>
+                            <p>sea, boat, photo</p>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="hover-state">
+                            <div className="hover-buttons align-center space-evenly make-flex">
+                                <a href="#" className="btn btn-primary with-icon make-flex">
+                                    <span className="icon"><img src={DetailsIcon} /></span>
+                                    <span className="text">Details</span>
+                                </a>
+                                <a href="#" className="btn btn-primary">View Live</a>                                
+                            </div>
+                        </div>
+
+                        <img className="main-image" src={IAmGIA} />
+
+                        <div className="information">
+                            <h5>yellow tea and flowers</h5>
+                            <p>sea, boat, photo</p>
+                        </div>
+                    </div>                                                                                
+
             	</div>
             </section>
         )
