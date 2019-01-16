ae4d1c978b047c24dcb8f552fb10b8ab11ad9ab8
Aakash Bhatia <aakash@arkade.com.au>
2019-01-16 14:05:45 +0530

    react modal added

--- src/js/organisms/Portfolio.js
+++ src/js/organisms/Portfolio.js
@@ -6,7 +6,10 @@ const PORTFOLIO = [
   { category: ["Vue", "Shopify", "Laravel", "Webpack"], name: "I.AM.GIA", text:"eCommerce", "image": "iamgia-1.jpg" },
   { category: ["Shopify", "Laravel", "Webpack"], name: "Decjuba", text:"eCommerce", "image": "decjuba-1.jpg" },
   { category: ["Vue", "Shopify", "Laravel", "Webpack"], name: "Zomp", text:"eCommerce", "image": "zomp-1.jpg" },
-  { category: ["WordPress", "React", "Webpack", "Gulp"], name: "Impromy", text:"Content Driven & Custom Apps", "image": "impromy-1.jpg" }
+  { category: ["WordPress", "React", "Webpack", "Gulp"], name: "Impromy", text:"Content Driven & Custom Apps", "image": "impromy-1.jpg" },
+  { category: ["React", "Webpack"], name: "Vpass Express", text:"iPad Visitor Management System", "image": "vpass-1.jpg" },
+  { category: ["Shopify", "React", "Webpack"], name: "Thr1ve Me", text:"eCommerce", "image": "thrive-1.jpg" },
+  { category: ["Laravel", "Preact", "Webpack"], name: "Glue", text:"Linking Qantas Frequent Flyer miles", "image": "glue-1.jpg" }    
 ];
 
 export default class extends React.Component {
